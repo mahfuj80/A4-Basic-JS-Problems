700fbd903f85c72a6a19413b896c6d0bc7f6d25a
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-09 00:23:29 +0600

    find from string added

---
+++ P2-Find-From-str.js
@@ -0,0 +1,7 @@
+function matchFinder(string1, string2) {
+    if (typeof string1 !== 'string' || typeof string2 !== 'string') {
+        return 'Invalid!, Please Input String';
+    } else {
+        return string1.includes(string2);
+    }
+}
